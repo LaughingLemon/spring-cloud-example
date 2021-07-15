4f496ab0c768d236981a449f5a6f59ec5fdd281c
LaughingLemon <laughing.programmer@outlook.com>
2021-07-15 21:40:06 +0100

    Initial commit

---
+++ basic-microservice/src/main/java/org/lemon/basic/microservice/BasicMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.lemon.basic.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicroserviceApplication.class, args);
+	}
+
+}
---
+++ basic-microservice/src/test/java/org/lemon/basic/microservice/BasicMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lemon.basic.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
