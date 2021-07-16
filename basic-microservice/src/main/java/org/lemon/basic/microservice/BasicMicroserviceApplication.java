95d0276b103f1c47098dad1ad1a420335dc2713e
LaughingLemon <laughing.programmer@outlook.com>
2021-07-16 21:03:36 +0100

    Enable Eureka Service and connect microservice

--- basic-microservice/src/main/java/org/lemon/basic/microservice/BasicMicroserviceApplication.java
+++ basic-microservice/src/main/java/org/lemon/basic/microservice/BasicMicroserviceApplication.java
@@ -2,8 +2,10 @@ package org.lemon.basic.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BasicMicroserviceApplication {
 
 	public static void main(String[] args) {
