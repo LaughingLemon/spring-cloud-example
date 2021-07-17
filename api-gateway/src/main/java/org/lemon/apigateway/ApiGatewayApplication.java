220600e6208982d74b45fb51e1dbedbda712f2a8
LaughingLemon <laughing.programmer@outlook.com>
2021-07-17 17:51:18 +0100

    Route API gateway requests to basic microservice using Eureka ("lb") and named service ("basic-microservice")

--- api-gateway/src/main/java/org/lemon/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/lemon/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,22 @@ package org.lemon.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/**")
+						.uri("lb://basic-microservice"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
